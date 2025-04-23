c967e44e775e1ae87835cd9db5dbc4fa7d74cade
Archit Xavier <architxavier4@gmail.com>
2025-04-23 16:01:58 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from backend import auth, upload, main
+from backend.auth import router as auth_router
+from backend.upload import router as upload_router
+from backend.main import run_main_app  # if it's defined in main.py
 
 import streamlit as st
 from backend.main import run_main_app  # assuming you defined a run_main_app() in main.py
